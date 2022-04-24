42e75a275451ea1433bc746058f5e8e0f5c651f7
VasiliyBogdanov <moproq@icloud.com>
2022-04-24 17:53:42 +0300

    Step 2
    Implement img downloading in png and jpg formats, make tests

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,48 +1,73 @@
-import requests
+from bs4 import BeautifulSoup
 import os
 import pathlib
+import requests
 import string
 from urllib.parse import urlparse
 
 
 def download(url: str, directory: str = None) -> str:
-    """
-    Downloads web page
-    :param url: Full URL
-    :param directory: Path to save downloaded file, default is cwd.
-    Must be existing directory or OSError will be raised.
-    :return: Full path to saved file
-    """
     if directory is None:
         directory = os.getcwd()
     if not pathlib.Path(directory).exists():
         raise OSError("Directory does not exist")
 
     content = requests.get(url).text
-    write_directory = os.path.join(directory, format_filename(url))
 
-    with open(write_directory, mode='w') as f:
-        f.write(content)
+    html_path = os.path.join(directory, format_filename(url, ext='.html'))
+    files_suffix = format_filename(url, ext='_files')
+    files_path = os.path.join(directory, files_suffix)
 
-    return write_directory
+    # Create directory for downloaded files
+    if not pathlib.Path(files_path).exists():
+        os.mkdir(files_path)
+
+    # Parse html for img tags
+    soup = BeautifulSoup(content, 'html.parser')
+    images = soup.find_all('img', recursive=True)
+
+    # Download img, if .png or .jpg
+    session = requests.Session()
+    for i in images:
+        image_path, image_ext = os.path.splitext(i['src'])
+        if image_ext in ('.png', '.jpg'):
+            image_name = format_filename(image_path[1:], image_ext)
+            formatted_img_path = i['src'][1:] \
+                if str(i['src']).startswith('/') \
+                else i['src']
+            img_download_path = os.path.join(url, formatted_img_path)
+            img_save_path = os.path.join(files_path, image_name)
+
+            # Download image
+            img_to_save = session.get(img_download_path, stream=True)
+
+            # Save downloaded image
+            save_img(img_to_save, img_save_path)
+
+            # Modify img links inside downloaded html
+            img_path_modified = os.path.join(files_suffix, image_name)
+            i['src'] = img_path_modified
+
+    # Save modified html
+    with open(html_path, mode='w') as output_html:
+        output_html.write(soup.prettify())
+
+    return html_path
 
 
-def format_filename(url: str) -> str:
-    """
-    Makes filename from full URL.
-    Example:
-    url = https://www.google.com
-    Function returns: www-google-com.html
-    :param url: Full URL
-    :return: Formatted filename
-    """
+def save_img(img, path):
+    with open(path, mode='wb') as f:
+        f.write(img.content)
+
+
+def format_filename(url: str, ext: str) -> str:
     parsed_url = urlparse(url, scheme='http')
 
     url_path = os.path.splitext(parsed_url.path)[0]
     url_path = url_path[:-1] if url_path.endswith('/') else url_path
 
     path_str = "".join((parsed_url.netloc, url_path))
-    formatted_url = ''.join(i if i not in string.punctuation
-                            else '-'
-                            for i in path_str) + '.html'
-    return formatted_url
+    filename = ''.join(i if i not in string.punctuation
+                       else '-'
+                       for i in path_str) + ext
+    return filename
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -4,24 +4,54 @@ import requests_mock
 import tempfile
 
 
+def read_file(path, mode='r'):
+    with open(path, mode=mode) as f:
+        return str(f.read()).strip()
+
+
 # Test format_name
-def test_format_name():
+def test_format_filename():
     url_without_extension = 'https://ru.hexlet.io/courses'
     url_with_extension = 'https://ru.hexlet.io/courses/about.html'
 
-    assert format_filename(url_without_extension) == 'ru-hexlet-io-courses.html'
-    assert format_filename(url_with_extension) == 'ru-hexlet-io-courses-about.html'
+    assert format_filename(url_without_extension, '.html') == 'ru-hexlet-io-courses.html'
+    assert format_filename(url_with_extension, '.html') == 'ru-hexlet-io-courses-about.html'
 
 
 # Test download
 def test_download():
-    test_data = 'data'
+    test_data_before = read_file('tests/fixtures/test_html_before.html')
+    test_data_after = read_file('tests/fixtures/test_html_after.html')
+    test_img_png = read_file('tests/fixtures/python.png', mode='rb')
+    test_img_jpg = read_file('tests/fixtures/python.jpg', mode='rb')
     test_url = 'https://ru.hexlet.io/courses'
+
     with requests_mock.Mocker() as m:
-        m.get(test_url, text=test_data)
+        m.get(test_url, text=test_data_before)
+        m.get('https://ru.hexlet.io/courses/assets/python.png', text=test_img_png)
+        m.get('https://ru.hexlet.io/courses/assets/python.jpg', text=test_img_jpg)
         with tempfile.TemporaryDirectory() as tmpdirname:
             result = download(test_url, tmpdirname)
-            assert result == os.path.join(tmpdirname, format_filename(test_url))
-            with open(result, mode='r') as f:
-                data = f.read()
-                assert data == test_data
+
+            # Test correctness of .html creation path
+            assert result == os.path.join(tmpdirname, format_filename(test_url, '.html'))
+
+            # Test correctness of created .html structure
+            result_html_data = read_file(result)
+            assert test_data_after == result_html_data
+
+            # Test that .html file and 'files' folder were created
+            assert format_filename(test_url, '.html') in os.listdir(tmpdirname)
+            assert format_filename(test_url, '_files') in os.listdir(tmpdirname)
+
+            # Test, that img was downloaded
+            downloaded_image_path = os.path.join(tmpdirname, os.listdir(tmpdirname)[0])
+            downloaded_image_dir = os.listdir(downloaded_image_path)
+            assert 'assets-python.png' in downloaded_image_dir
+            assert 'assets-python.jpg' in downloaded_image_dir
+
+            # Test correctness of downloaded img
+            downloaded_png = read_file(os.path.join(downloaded_image_path, downloaded_image_dir[0]))
+            downloaded_jpg = read_file(os.path.join(downloaded_image_path, downloaded_image_dir[1]))
+            assert downloaded_png == test_img_png
+            assert downloaded_jpg == test_img_jpg
